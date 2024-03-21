784c580561b802f7a94a258df39c821c8be1b650
Philip Nash <siliconrayder@proton.me>
2024-03-21 17:16:02 -0400

    Better exception handling

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -1,17 +1,19 @@
 # ForecastController is the only controller in the application
 # Defines :index action to enter any address
 # Defines :show action to display current and daily forecasts
+# :show displays an error message if an exception bubbles up
 
 class ForecastController < ActionController::Base
   def index; end
 
   def show
-    raise 'Address parameter not supplied' unless params[:address]
-
-    @forecast = Forecast.new(params[:address]).forecast
-  rescue StandardError => e
-    @error = e.to_s
-    status = params[:address].nil? ? 400 : 500
-    render :show, status: status
+    begin
+      raise ArgumentError.new('Address parameter not supplied') unless params[:address].present?
+      @forecast = Forecast.new(params[:address]).forecast
+    rescue => e
+      status = e.instance_of?(ArgumentError) ? 400 : 500
+      @error = e.message
+      render :show, status: status
+    end
   end
 end
